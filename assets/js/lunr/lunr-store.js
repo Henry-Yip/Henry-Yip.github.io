8387a5c3c7ccd15cbb298873498a4353cbd103d5
Henry Yip <betelgeuse0125@gmail.com>
2022-08-03 13:45:05 +0800

    Edit

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -185,10 +185,16 @@ var store = [{
         "url": "/Music/2022-05-20-ClassicalMusic/",
         "teaser": "/assets/images/Music.png"
       },{
-        "title": "Astrophysics",
+        "title": "Astrophysics (Part 1)",
         "excerpt":"Introduction This set of notes is made with reference to the Astrophysics Coursebook by The University of Edinburgh. I have gained permission from Professor Catherine Heymans to put my modified notes in my website. It is also available in my GitHub account. Prerequisites Proficiency in Calculus and Classical Physics, in...","categories": [],
         "tags": ["Physics"],
-        "url": "/Physics/Astrophysics/",
+        "url": "/Physics/Astrophysics1/",
+        "teaser": "/assets/images/Astro.png"
+      },{
+        "title": "Astrophysics (Part 2)",
+        "excerpt":"2. Overleaf Overleaf is an online, collaborative LaTeX Editor. Downloading the documents Go to the offical website of Overleaf and register for a new account. Create a new project and copy all the lines in main.tex (the document above). Press compile and download the pdf document. 3. TexMaker Download TexMaker...","categories": [],
+        "tags": ["Physics"],
+        "url": "/Physics/Astrophysics2/",
         "teaser": "/assets/images/Astro.png"
       },{
         "title": "Beyond a cursory look at the structure of the Lorentz factor (Leo Chow)",
