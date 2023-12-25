7a8a570ef77b7f0bcdf81fc3a0c34a44990d6bcf
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2023-12-25 23:21:28 +0000

    Update

--- _site/startTerminal.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-bundle exec jekyll build
-git add -A 
-git commit -m "Automated"
-git push
-pause
--- startTerminal.bat
+++
@@ -1,6 +0,0 @@
-cd /d %~dp0
-start bundle exec jekyll build
-start git add -A 
-start git commit -m "Automated"
-start git push
-pause
