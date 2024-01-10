3136865d5c062194a914305727f4565d79dba9f2
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2024-01-10 20:52:10 +0000

    Another Update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,121 @@
+var store = [{
+        "title": "LaTeX Tutorial (1)",
+        "excerpt":"Why \\(\\LaTeX\\) According to Overleaf: “LATEX is used all over the world for scientific documents, books, as well as many other forms of publishing. Not only can it create beautifully typeset documents, but it allows users to very quickly tackle the more complicated parts of typesetting, such as inputting mathematics,...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/LaTeXTutorial1/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "LaTeX Tutorial (2)",
+        "excerpt":"Tex Source This is the Tex Source for my beamer presentation \\documentclass{beamer} \\usepackage[utf8]{inputenc} \\usepackage{amsmath} \\usepackage{parskip} \\usepackage{amssymb} \\usepackage{hyperref} \\usepackage{float} \\hypersetup{ colorlinks=true, linkcolor=blue, filecolor=magenta, urlcolor=cyan, pdftitle={Overleaf Example}, pdfpagemode=FullScreen, } \\usetheme{Madrid} \\usecolortheme{default} \\usepackage{xspace} %------------------------------------------------------------ %This block of code defines the information to appear in the %Title page \\title[Introduction to \\LaTeX] %optional {Introduction to...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/LaTeXTutorial2/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "LaTeX Tutorial (3)",
+        "excerpt":"Introuduction   A standardized \\(\\LaTeX\\) document   If you can’t view it below please press HERE     ","categories": [],
+        "tags": [],
+        "url": "/LaTeX/LaTeXTutorial3/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Sample Tex Files (1)",
+        "excerpt":"The Product HERE The Source Code \\documentclass{article} \\usepackage{amsmath} \\usepackage{amssymb} \\usepackage{hyperref} \\hypersetup{ colorlinks=true, linkcolor=red, urlcolor=cyan } \\title{\\textbf{A Brief Introduction to this Website}} \\author{ Henry Yip s2231321@ed.ac.uk } \\begin{document} \\maketitle \\begin{abstract} \\begin{itemize} \\item This is a self-initiated project by Henry Yip. Feel free to send me any corrections. \\end{itemize} \\end{abstract} \\section{Aim} \\begin{itemize}...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/Tex1/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Sample Tex Files (2)",
+        "excerpt":"The Product: HERE Please note that the math are very non-rigorous… I don’t even know what I was doing \\documentclass{article} \\usepackage[utf8]{inputenc} \\usepackage{parskip} \\usepackage{amsmath} \\usepackage{amssymb} \\usepackage[thinc]{esdiff} \\usepackage{tikz} \\usetikzlibrary{calc} \\title{2nd order LODE} \\author{ \\textbf{Henry Yip} and \\textbf{Lorian Richmond}\\\\ } \\date{March 2022} \\begin{document} \\maketitle \\begin{abstract} This appendix is related to solving ordinary diffential...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/Tex2/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Astrophysics 1",
+        "excerpt":"Astrophysics Course Notes Hi Everyone, this set of notes is made with reference to the Astrophysics Coursebook by The University of Edinburgh. I have gained permission from Professor Catherine Heymans to put my modified notes in my website. Prerequisites Proficiency in Calculus and Classical Physics, in particular, Newtonian Mechanics. Scope...","categories": [],
+        "tags": ["Astrophysics","1"],
+        "url": "/Physics/Astrophysics-1/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Astrophysics 2",
+        "excerpt":"This is the PDF I will also send you the tex code. Below I will show you how to use! 1. Overleaf Overleaf is an online, collaborative LaTeX Editor. Downloading the documents Go to the offical website of Overleaf and register for a new account. Create a new project and...","categories": [],
+        "tags": ["Astrophysics","2"],
+        "url": "/Physics/Astrophysics-2/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Allison Lau - Numerical Methods",
+        "excerpt":"Allison Lau   Allison Lau is a student from the University of Toronto. These are the notes she wrote for her Computer Science course. If you have any suggestions/amendments, please send her!   Please Press HERE to download!  ","categories": [],
+        "tags": ["Allison","Lau","-","Numerical","Methods"],
+        "url": "/Physics/Allison-Lau",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Leo Chow - Relativity",
+        "excerpt":"Author Introduction Leo Chow is a student in the University of Hong Kong, studying Mathematics and Physics He has personally permitted me to upload his blog in my website. Relativity Let’s take a look at the Lorentz factor: \\[\\gamma_v=\\frac{1}{\\sqrt{1-\\frac{v^2}{c^2}}}\\] On the right hand side is a recurring structure in the...","categories": [],
+        "tags": ["Astrophysics","1"],
+        "url": "/Physics/Leo-Chow",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Physics Book I read before university",
+        "excerpt":"The Book This is the link to the first physics book I ever received: HERE! (Obviously I’m not sending the book to you I’m just recommending it to you) It’s nice introduction to Quantum Physics, even though some math are definitely missing, it does provide some sort of analogies and...","categories": [],
+        "tags": ["Physics","Book"],
+        "url": "/Physics/FirstBook/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Appendix",
+        "excerpt":"Appendix B Used in my post about HKDSE Physics Proposition 1.2.1: $$r^2 \\dot{\\theta} \\equiv h=\\frac{ \\underline{L} }{m}$$ Proof Going back to Planar Polar Coordinates: \\[\\hat{r}=\\cos (\\theta) \\hat{x}+\\sin (\\theta) \\hat{y}, \\quad \\hat{\\theta}=-\\sin (\\theta) \\hat{x}+\\cos (\\theta) \\hat{y} .\\] By Chain Rule, \\[\\dot{\\hat{r}}=\\dot{\\theta}[-\\sin (\\theta) \\hat{x}+\\cos (\\theta) \\hat{y}]=\\dot{\\theta} \\hat{\\theta}, \\quad \\dot{\\hat{\\theta}}=\\dot{\\theta}[-\\cos (\\theta) \\hat{x}-\\sin (\\theta)...","categories": [],
+        "tags": ["Appendix"],
+        "url": "/Physics/Appendix/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Classical Mechanics in HKDSE context",
+        "excerpt":"Note: Some of the content are similar to lecture notes from my Dynamics courses, Introductory Dynamics (PHYS08052) and Lagrangian Dynamics (PHYS10015). I tried my best to avoid copying, but some of the flow may + similar. Intended Audience: Year 1 Math/Physics major. Motivation HKDSE is a public exam taken by...","categories": [],
+        "tags": ["HKDSE"],
+        "url": "/Physics/HKDSE/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Small Pertubations",
+        "excerpt":"Orbit Equation: Using the knowledge from Appendix B, we can quite easily deduce the orbit equation: \\[\\ddot{r}-\\frac{h^2}{r^3}=\\frac{F_r(r)}{m}\\] I’ll leave this as an exercise for the reader. Hint 1: We're dealing with Central Force, only radial force matters Hint 2: Use Newton's Second Law I enjoy the physics of small perbuations,...","categories": [],
+        "tags": ["Small","Pertubations"],
+        "url": "/Physics/SmallPertubations/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Analysis/Fourier Series",
+        "excerpt":"Start Here if you don’t know Fourier Series A fourier series decomposes a function into trigonometric functions. By adding more and more trigonometric functions together, the summation itself looks more and more like the original function. Look at the graph below, does it look like a sawtooth over time? The...","categories": [],
+        "tags": ["Analysis/Fourier","Series"],
+        "url": "/Physics/Analysis_Fourier/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Cosets and Lagrange",
+        "excerpt":"Cosets Theorem Claim: \\(g_1H=g_2H\\) is possible even when \\(g_1 \\neq g_2\\) Proof: Consider a subgroup of \\(S_3\\) that only contains the rotations and the identity element \\(e\\). From above, this subgroup contains \\((n-1)+1=n\\) elements , so the subgroup, denoted by \\(H\\), has \\(3\\) elements, i.e. \\(e, (123),(132)\\). Now consider \\((12)...","categories": [],
+        "tags": ["Cosets","and","Lagrange"],
+        "url": "/Physics/CosetsandLagrange/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Different Types of Groups",
+        "excerpt":"Dihedral Groups The symmetries of an n-gon form a group (Proved in Prerequisites) We call it Dihedral Group Theorem 1.1 The dihedral group \\(D_n\\), has precisely \\(\\left|D_n\\right|=2 n\\) elements Proof \\(D_n\\) consists of: The identity \\(e \\in D_n\\) (Trivial) The \\(n-1\\) rotations (any direction) through angles \\(k \\cdot \\frac{2 \\pi}{n}\\),...","categories": [],
+        "tags": ["Different","Types","of","Groups"],
+        "url": "/Physics/DiffTypesofGroups/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Pre-requisites for Group Theory",
+        "excerpt":"Equivalence Class Why Equivalence Classes? To understand Cosets, hence understand very important theorems like Lagrange’s Theorem Consider the natural numbers \\(\\mathbb{N}\\). Consider the remainder when divided by 3. It is clear that some numbers has remainder 0, some has remainder 1, and the rest has remainder 2. We can group...","categories": [],
+        "tags": ["Pre-requisites","for","Group","Theory"],
+        "url": "/Physics/Pre-requisites_for_Group_Theory/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Group Theory",
+        "excerpt":"A gentle introduction to Group Theory, with a heavy emphasis on intuitions.   Pre-requisites: Introduction to graphs, group axioms, equivalence class, etc     Pre-requisites   Different Types of Groups: Dihedral Groups, Symmetric Groups, etc; Subgroups, Normal Subgroups     Types of Groups   Cosets; Lagrange’s Theorem and its properties; Intuitions behind     Cosets; Lagrange’s Theorem   ","categories": [],
+        "tags": ["Group","Theory"],
+        "url": "/Physics/Group%20Theory/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Multivariable Calculus",
+        "excerpt":"Targeted Audience: Year 1/2 Math/Physics majors. You may want to read a bit about wedge products and exterior derivatives Multivariable Calculus is a prerequisite. Statement An Ellipse with semi-major axis a and semi-minor axis b has area \\(\\pi ab\\) Proof There are many ways to approach this problem. Proofwiki provides...","categories": [],
+        "tags": ["Multivaribale","Calculus"],
+        "url": "/Physics/Multivaribale%20Calculus/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Einstein Summation",
+        "excerpt":"Disclaimer This page is directly inspired (many parts copied) from my Geometry Course Notes, written by Dr Johans Martens. Introduction Einstein Summation has been very important in many part of physics, especially where we need to sum up a lot of stuff, like in electromagnetism. Mathematicians define it more rigorously...","categories": [],
+        "tags": ["Einstein","Summation"],
+        "url": "/Physics/Einstein%20Summation/",
+        "teaser": "/assets/images/Teaser.png"
+      }]
