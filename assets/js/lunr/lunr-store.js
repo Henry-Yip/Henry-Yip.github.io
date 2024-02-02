6602bc0e1ae17413cb056e0b1ff8dca3f2cf0e85
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2024-02-02 00:26:27 +0000

    assets change; fixing broken URLs

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{
+        "title": "Henry Yip",
+        "excerpt":"Photo My name is Henry Yip, a Year 3 Mathematical Physics Student at the University of Edinburgh. I am interested in Classical Mechanics and Differential Geometry, and I will do a supervised funded summer research on 3-body problem (in Classical Mechanics) this summer. I am a course representative for Mathematical...","categories": [],
+        "tags": [],
+        "url": "/Contributors/HenryYip/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "LaTeX Tutorial (Presentation)",
+        "excerpt":"This is a beamer presentation I made on using \\(\\LaTeX\\). Please note that I am not a \\(\\LaTeX\\) expert, and this is for reference(for fun) only!   Tutorial      ","categories": [],
+        "tags": [],
+        "url": "/LaTeX/LaTeXTutorial1/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "LaTeX Tutorial (Source Code of Presentation)",
+        "excerpt":"Tex Source This is the Tex Source for my beamer presentation \\documentclass{beamer} \\usepackage[utf8]{inputenc} \\usepackage{amsmath} \\usepackage{tikz} \\usepackage{xcolor} \\definecolor{logoBlue}{RGB}{41, 128, 185} \\definecolor{logoText}{RGB}{44, 62, 80} \\usetikzlibrary{calc} \\usepackage{parskip} \\usepackage{amssymb} \\usepackage{hyperref} \\usepackage{float} \\hypersetup{ colorlinks=true, linkcolor=blue, filecolor=magenta, urlcolor=cyan, pdftitle={Overleaf Example}, pdfpagemode=FullScreen, } \\usetheme{Madrid} \\usecolortheme{default} \\usepackage{xspace} %------------------------------------------------------------ %This block of code defines the information to appear in...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/LaTeXTutorial2/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "LaTeX Tutorial (Article Form)",
+        "excerpt":"Introuduction   A standardized \\(\\LaTeX\\) document   If you can’t view it below please press HERE     ","categories": [],
+        "tags": [],
+        "url": "/LaTeX/LaTeXTutorial3/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Sample Tex Files (1)",
+        "excerpt":"Here we include more sample Tex Files for reference! The Product HERE The Source Code \\documentclass{article} \\usepackage{amsmath} \\usepackage{amssymb} \\usepackage{hyperref} \\hypersetup{ colorlinks=true, linkcolor=red, urlcolor=cyan } \\title{\\textbf{A Brief Introduction to this Website}} \\author{ Henry Yip s2231321@ed.ac.uk } \\begin{document} \\maketitle \\begin{abstract} \\begin{itemize} \\item This is a self-initiated project by Henry Yip. Feel free...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/Tex1/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Sample Tex Files (2)",
+        "excerpt":"This includes mathematics equations which suits people creating \\(\\LaTeX\\) documents to do their problem sheets/hand-in Please Press HERE \\documentclass{article} \\usepackage{amsmath} \\usepackage{amssymb} \\usepackage{parskip} \\usepackage [utf8] {inputenc} \\newcommand{\\ie}{\\textit{i}.\\textit{e}. } \\title{Introductory Astrophysics (PHYS08050) Notes} \\author{Henry Yip s2231321@ed.ac.uk } \\begin{document} \\maketitle \\begin{abstract} This set of notes is made with reference to the Astrophysics Coursebook...","categories": [],
+        "tags": [],
+        "url": "/LaTeX/Tex2/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "A Fun Astrophysics Article on Dark Energy",
+        "excerpt":"Below is something I wrote for fun/to practice my \\(\\LaTeX\\) skills only! Please don’t take it too seriously!   PDF download  Please Press HERE       ","categories": [],
+        "tags": ["Astrophysics","1"],
+        "url": "/Astrophysics/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Leo Chow - Relativity",
+        "excerpt":"This set of notes is from Leo Chow, a student in the University of Hong Kong, on special relativity. I have met him in person and he has personally permitted me to upload his blog in my website. Relativity Let’s take a look at the Lorentz factor: \\[\\gamma_v=\\frac{1}{\\sqrt{1-\\frac{v^2}{c^2}}}\\] On the...","categories": [],
+        "tags": ["Astrophysics","1"],
+        "url": "/LeoChow/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Allison Lau - Numerical Methods",
+        "excerpt":"This set of notes is on numerical methods, created by Allison Lau, a student from University of Toronto. I have met her in person and she has personally provided me this set of notes to put in my website.   Please Press HERE to download!  ","categories": [],
+        "tags": ["Allison","Lau","-","Numerical","Methods"],
+        "url": "/Numerical/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "PHYS08050 (Introductory Astrophysics) Course Notes",
+        "excerpt":"This set of notes is made with reference to the Astrophysics Coursebook by The University of Edinburgh. There are many errors and I am not free enough to proofread through it, but please send me corrections if you do find some! I have gained permission from Professor Catherine Heymans to...","categories": [],
+        "tags": ["Astrophysics","1"],
+        "url": "/Physics/Astrophysics-1/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Conservations in Physics - Noether's Theorem",
+        "excerpt":"Many things are constant (conserved) in Physics. For example, energy is conserved, center of mass is conserved… Below I will given an example: \\[\\hat{r}=\\cos (\\theta) \\hat{x}+\\sin (\\theta) \\hat{y}, \\quad \\hat{\\theta}=-\\sin (\\theta) \\hat{x}+\\cos (\\theta) \\hat{y} .\\] By Chain Rule, \\[\\dot{\\hat{r}}=\\dot{\\theta}[-\\sin (\\theta) \\hat{x}+\\cos (\\theta) \\hat{y}]=\\dot{\\theta} \\hat{\\theta}, \\quad \\dot{\\hat{\\theta}}=\\dot{\\theta}[-\\cos (\\theta) \\hat{x}-\\sin (\\theta) \\hat{y}]=-\\dot{\\theta}...","categories": [],
+        "tags": ["Appendix"],
+        "url": "/Conservations/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Classical Mechanics in HKDSE context",
+        "excerpt":"This is a post on Classical Mechanics in High School style. It serves as a good introduction to High Schoolers wanting to pursue a deeper understanding in Physics. Note: Some of the content are similar to lecture notes from my Dynamics courses, Introductory Dynamics (PHYS08052) and Lagrangian Dynamics (PHYS10015). I...","categories": [],
+        "tags": ["HKDSE"],
+        "url": "/HKDSE/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Perturbations - A view on Classical Mechanics and Quantum Mechanics",
+        "excerpt":"“Perturbations” have vastly different meanings in classical mechanics and quantum physics. They both mean a small “deviation” from the origin, however. Note: The notes here are very heavily inspired by the notes written by Prof Arjun Berera, who has taught me both Introductory Dynamics (PHYS08052) and Principles of Quantum Mechanics...","categories": [],
+        "tags": ["Small","perturbations"],
+        "url": "/SmallPerbuations/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Kernels in Fourier Series",
+        "excerpt":"This is an introduction to Fourier Series. I am trying to understand Dirichlet’s Kernel taught in class. Disclaimer: Some of the content below are inspired by my Honours Analysis Notes and Introductory Waves Notes! Start Here if you don’t know Fourier Series A fourier series decomposes a function into trigonometric...","categories": [],
+        "tags": ["Analysis/Fourier","Series"],
+        "url": "/Dirichlet/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Pre-requisites for Group Theory",
+        "excerpt":"Group Theory is one of the most important theories in Physics. Here we try to complete a basic introduction to it, or at least complete the background learning to facilitate its learning. Equivalence Class Why Equivalence Classes? To understand Cosets, hence understand very important theorems like Lagrange’s Theorem Consider the...","categories": [],
+        "tags": ["Pre-requisites","for","Group","Theory"],
+        "url": "/Prerequisites/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Multivariable Calculus",
+        "excerpt":"Here we are using the language of differential geometry to explore around Green’s Theorem and Stokes’ Theorem. Targeted Audience: Year 1/2 Math/Physics majors. You may want to read a bit about wedge products and exterior derivatives Multivariable Calculus is a prerequisite. Statement An Ellipse with semi-major axis a and semi-minor...","categories": [],
+        "tags": ["Multivaribale","Calculus"],
+        "url": "/EllipseArea/",
+        "teaser": "/assets/images/Teaser.png"
+      }]
