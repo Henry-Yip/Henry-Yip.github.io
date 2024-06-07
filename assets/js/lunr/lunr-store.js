fa071c924b5767d555c609d3b2f27067ee55407d
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2024-06-08 01:42:09 +0800

    Error Fixed

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,84 +1,157 @@
----
-layout: none
----
-
-var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | relative_url | jsonify }},
-        "teaser": {{ teaser | relative_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}{%- if site.lunr.search_within_pages -%},
-  {%- assign pages = site.pages | where_exp:'doc','doc.search != false' -%}
-  {%- for doc in pages -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-  {
-    "title": {{ doc.title | jsonify }},
-    "excerpt":
-        {%- if site.search_full_content == true -%}
-          {{ doc.content | newline_to_br |
-            replace:"<br />", " " |
-            replace:"</p>", " " |
-            replace:"</h1>", " " |
-            replace:"</h2>", " " |
-            replace:"</h3>", " " |
-            replace:"</h4>", " " |
-            replace:"</h5>", " " |
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | jsonify }},
-        {%- else -%}
-          {{ doc.content | newline_to_br |
-            replace:"<br />", " " |
-            replace:"</p>", " " |
-            replace:"</h1>", " " |
-            replace:"</h2>", " " |
-            replace:"</h3>", " " |
-            replace:"</h4>", " " |
-            replace:"</h5>", " " |
-            replace:"</h6>", " "|
-          strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-        {%- endif -%}
-      "url": {{ doc.url | absolute_url | jsonify }}
-  }{%- unless forloop.last and l -%},{%- endunless -%}
-  {%- endfor -%}
-{%- endif -%}]
+var store = [{
+        "title": "LaTeX Tutorial (Presentation)",
+        "excerpt":"This is a beamer presentation I made on using \\(\\LaTeX\\). Please note that I am not a \\(\\LaTeX\\) expert, and this is for reference(for fun) only!   Tutorial   Please Press HERE      ","categories": [],
+        "tags": ["Latex"],
+        "url": "/LaTeX/LaTeXTutorial1/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "LaTeX Tutorial (Source Code of Presentation)",
+        "excerpt":"Tex Source This is the Tex Source for my beamer presentation \\documentclass{beamer} \\usepackage[utf8]{inputenc} \\usepackage{amsmath} \\usepackage{tikz} \\usepackage{xcolor} \\definecolor{logoBlue}{RGB}{41, 128, 185} \\definecolor{logoText}{RGB}{44, 62, 80} \\usetikzlibrary{calc} \\usepackage{parskip} \\usepackage{amssymb} \\usepackage{hyperref} \\usepackage{float} \\hypersetup{ colorlinks=true, linkcolor=blue, filecolor=magenta, urlcolor=cyan, pdftitle={Overleaf Example}, pdfpagemode=FullScreen, } \\usetheme{Madrid} \\usecolortheme{default} \\usepackage{xspace} %------------------------------------------------------------ %This block of code defines the information to appear in...","categories": [],
+        "tags": ["Latex"],
+        "url": "/LaTeX/LaTeXTutorial2/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "LaTeX Tutorial (Article Form)",
+        "excerpt":"Introuduction   A standardized \\(\\LaTeX\\) document   If you can’t view it below please press HERE     ","categories": [],
+        "tags": ["Latex"],
+        "url": "/LaTeX/LaTeXTutorial3/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Sample Tex Files (1)",
+        "excerpt":"Here we include more sample Tex Files for reference! The Product HERE The Source Code \\documentclass{article} \\usepackage{amsmath} \\usepackage{amssymb} \\usepackage{hyperref} \\hypersetup{ colorlinks=true, linkcolor=red, urlcolor=cyan } \\title{\\textbf{A Brief Introduction to this Website}} \\author{ Henry Yip s2231321@ed.ac.uk } \\begin{document} \\maketitle \\begin{abstract} \\begin{itemize} \\item This is a self-initiated project by Henry Yip. Feel free...","categories": [],
+        "tags": ["Latex"],
+        "url": "/LaTeX/Tex1/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "Sample Tex Files (2)",
+        "excerpt":"This includes mathematics equations which suits people creating \\(\\LaTeX\\) documents to do their problem sheets/hand-in. The PDF can be downloaded here: My Page About Astrophysics Course Notes \\documentclass{article} \\usepackage{amsmath} \\usepackage{amssymb} \\usepackage{parskip} \\usepackage [utf8] {inputenc} \\usepackage[margin=0.8in]{geometry} \\newcommand{\\ie}{\\textit{i}.\\textit{e}. } \\title{Introductory Astrophysics (PHYS08050) Notes} \\author{Henry Yip s2231321@ed.ac.uk } \\begin{document} \\maketitle \\begin{abstract} This set...","categories": [],
+        "tags": ["Latex"],
+        "url": "/LaTeX/Tex2/",
+        "teaser": "/assets/images/LaTeX.png"
+      },{
+        "title": "A Fun Astrophysics Article on Dark Energy",
+        "excerpt":"Below is something I wrote for fun/to practice my \\(\\LaTeX\\) skills only! Please don’t take it too seriously!   PDF download  Please Press HERE       ","categories": [],
+        "tags": [],
+        "url": "/Astrophysics/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Allison Lau - Numerical Methods",
+        "excerpt":"This set of notes is on numerical methods, created by Allison Lau, a student from University of Toronto. I have met her in person and she has personally provided me this set of notes to put in my website.   Please Press HERE to download!  ","categories": [],
+        "tags": [],
+        "url": "/Numerical/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Leo Chow - Relativity",
+        "excerpt":"This set of notes is from Leo Chow, a student in the University of Hong Kong, on special relativity. I have met him in person and he has personally permitted me to upload his blog in my website. Relativity Let’s take a look at the Lorentz factor: \\[\\gamma_v=\\frac{1}{\\sqrt{1-\\frac{v^2}{c^2}}}\\] On the...","categories": [],
+        "tags": [],
+        "url": "/Relativity/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "PHYS08050 (Introductory Astrophysics) Course Notes",
+        "excerpt":"This set of notes is made with reference to the Astrophysics Coursebook by The University of Edinburgh. There are many errors and I am not free enough to proofread through it, but please send me corrections if you do find some! I have gained permission from Professor Catherine Heymans to...","categories": [],
+        "tags": [],
+        "url": "/Physics/Astrophysics-1/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Two Laws Related to Planetary Revolution",
+        "excerpt":"This post is related to Kepler’s Third and Virial’s Theorem, twp famous laws related to planets revolution around the sun. Furthermore, Virial’s Theorem is very important in statistical mechanics and is seen in Quantum Mechanics also (for example in quantum harmonic oscillators). Kepler’s Third Law Using even high school knowledge...","categories": [],
+        "tags": ["Classical Mechanics"],
+        "url": "/KeplerVirial/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Conservations in Physics - Noether's Theorem",
+        "excerpt":"This article is built for two types of audiences. The first part is quite involved and assumes knowledge of advanced classical mechanics (for example Lagrangian Dynamics and Variational Principle). The second part is more mild and is targeted for students with some experiences in basic calculus skills. Proof of Noether’s...","categories": [],
+        "tags": ["Classical Mechanics"],
+        "url": "/NoetherTheorem/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Perturbations - A view on Classical Mechanics and Quantum Mechanics",
+        "excerpt":"“Perturbations” have vastly different meanings in classical mechanics and quantum physics. They both mean a small “deviation” from the origin, however. Note: The notes here are very heavily inspired by the notes written by Prof Arjun Berera, who has taught me both Introductory Dynamics (PHYS08052) and Principles of Quantum Mechanics...","categories": [],
+        "tags": ["Classical Mechanics","Quantum Mechanics"],
+        "url": "/SmallPerbuations/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Kernels in Fourier Series",
+        "excerpt":"This is an introduction to Fourier Series. I am trying to understand Dirichlet’s Kernel taught in class. Disclaimer: Some of the content below are inspired by my Honours Analysis Notes and Introductory Waves Notes! Start Here if you don’t know Fourier Series A fourier series decomposes a function into trigonometric...","categories": [],
+        "tags": [],
+        "url": "/Dirichlet/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Pre-requisites for Group Theory",
+        "excerpt":"Group Theory is one of the most important theories in Physics. Here we try to complete a basic introduction to it, or at least complete the background learning to facilitate its learning. Equivalence Class Why Equivalence Classes? To understand Cosets, hence understand very important theorems like Lagrange’s Theorem Consider the...","categories": [],
+        "tags": [],
+        "url": "/Prerequisites/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Two Interesting Questions related to Free Fall",
+        "excerpt":"This post involves two questions related to free falling. The first is about the terminal velocity of human and a plane under turbulent flow, and the second is about what happens if a human drops to the center of the earth. The results are highly fasicinating! Free Fall of objects...","categories": [],
+        "tags": ["Classical Mechanics"],
+        "url": "/FreeFall/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Two Particles Collision",
+        "excerpt":"Two point masses \\(m_1\\) and \\(m_2\\) are spaced apart in a vacuum in space, with no other forces present other than the gravitational force between them. At what time will they meet in the center? With object 1 on the left and object 2 on the right, we can define...","categories": [],
+        "tags": ["Classical Mechanics"],
+        "url": "/TwoParticles/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "More on Multivariable Calculus",
+        "excerpt":"This section covers the content usually omitted in Calculus 3 Area Estimation using Green’s Theorem An example using Generalized Stoke’s Theorem Curl and Divergence in generalized OCCs (Orthogonal Curvilinear Coordinates) Area Estimation using Green’s Theorem I will illustrate this section using an example: An ellipse with semi-major axis a and...","categories": [],
+        "tags": ["Calculus"],
+        "url": "/Multivariable_Calc/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Tensors and its transformations",
+        "excerpt":"Here, we introduce the definition of tensors and the visualization behind it. Then, we discuss its transformation to another basis with a worked example. Definition Consider a rotation of the \\(\\{e_i\\}\\) basis (frame \\(S\\)) to the \\(\\{e'_i\\}\\) basis (frame \\(S'\\)). This is called a passive rotation. The components of two...","categories": [],
+        "tags": ["Algebra"],
+        "url": "/TensorTransformations/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Overview of the Two-body Problem",
+        "excerpt":"Here I will present the exact solution of the two body problem. Definition of terms First we define the center of mass coordinate, \\(R\\): \\[\\underline{R}=\\frac{m_1 \\underline{r_1}+m_2 \\underline{r_2}}{m_1+m_2}\\] As \\(m_1\\) and \\(m_2\\) are invariant, we can express \\(P\\), the total momentum, in terms of \\(\\dot{R}\\): \\[\\underline{P}=(m_1+m_2)\\dot{R}\\] How does the center of...","categories": [],
+        "tags": ["Research","Classical Mechanics"],
+        "url": "/SummerResearch/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Self-Initiated Reserach Group Project",
+        "excerpt":"Liquid behaviour is hard to analyze and usually requires the Navier Stokes equation, which does not have an analytic solution. With reference to a published paper, we attempt to model liquid behaviour in the low velocity limit using two models: Continuous Model and Double Pendulum Model. Poster We gave a...","categories": [],
+        "tags": ["Research"],
+        "url": "/ESRAProject/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Julia Sets and the Mandelbrot Set",
+        "excerpt":"Here we explore the relationships between Filled-in Julia Sets and the Mandelbrot Set. In this article we consider the function: \\(f_c: z \\mapsto z^2+c\\), where \\(c\\) is a constant. We then define \\(K_c\\), which is also known as the Filled-in Julia Set, as the set of \\(z\\) which are non-escaping...","categories": [],
+        "tags": ["Fractal Geometry"],
+        "url": "/MandelbrotSet/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Why is Momentum Conserved?",
+        "excerpt":"In this article we explore the conservation of momentum using Lagrangian Dynamics. Momentum in Physics Momentum is defined by \\(p=mv\\). It is a vector quantity, meaning the direction matters. The derivative of momentum is force. In an inertial frame, momentum is a conserved quantity, meaning that it is not affected...","categories": [],
+        "tags": ["Classical Mechanics"],
+        "url": "/MomentumConservation/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Theorema Egregium --- The reason why all maps are wrong",
+        "excerpt":"Theorema Egregium states that isometric surfaces have the same gauss(ian) curvature. Equivalently, surfaces with different gauss curvature are not isometric (with same First Fundamental Forms). Gauss Curvature of a Sphere First note that the Gauss Curvature on the whole sphere is uniform. Next notice that the surface area of a...","categories": [],
+        "tags": ["Differential Geometry"],
+        "url": "/TheoremaEgregium/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Euler's Totient function, Reduced Totient function and Carmichael Number",
+        "excerpt":"This post is a gentle introduction on the famous theorems in Number Theory. Motivation I have been given this question in my first Mathematics Class on proving theorems, Accelerated Proofs and Problem Solving: Find \\(a\\) such that \\[3^{124} \\equiv a \\space \\text{mod} \\space 100\\] The answer is \\(81\\) upon my...","categories": [],
+        "tags": ["Number Theory"],
+        "url": "/CarmichaelFunction/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "Integration by Laplace Transform",
+        "excerpt":"Laplace Transform is usually used to “decompose” a function into its constituents, or to solve differential equations. However, it can also be used to solve integrals. Below, I will show how it can be used to prove: \\[\\int_0^{\\infty} \\frac{\\sin(x)}{x} \\, dx=\\frac{\\pi}{2}\\] Transforming the problem into a Laplace Transformation Problem Observe...","categories": [],
+        "tags": ["Calculus"],
+        "url": "/LaplaceTransforms/",
+        "teaser": "/assets/images/Teaser.png"
+      },{
+        "title": "An Application of the LTE lemma and Fermat's Little Theorem",
+        "excerpt":"Here, I am trying to determine all integers \\(n &gt; 1\\) such that \\(\\frac{2^n+1}{n^2}\\) is an integer. First Part Assume \\(a \\neq 2\\) is the smallest prime that divides \\(n\\). Then, since \\(a \\vert 2^n+1\\), \\(a \\vert 2^{2n}-1\\). Also, we know that \\(a \\vert 2^{a-1}-1\\) (Fermat’s Little Theorem) Therefore, \\(a...","categories": [],
+        "tags": ["Number Theory"],
+        "url": "/IMO/",
+        "teaser": "/assets/images/Teaser.png"
+      }]
