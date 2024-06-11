7ac6964d70ffdc76734374c251102d8809067bd4
Henry Yip <102658036+Henry-Yip@users.noreply.github.com>
2024-06-11 16:12:19 +0800

    Improved Tags

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -55,19 +55,19 @@ var store = [{
       },{
         "title": "Two Laws Related to Planetary Revolution",
         "excerpt":"This post is related to Kepler’s Third and Virial’s Theorem, twp famous laws related to planets revolution around the sun. Furthermore, Virial’s Theorem is very important in statistical mechanics and is seen in Quantum Mechanics also (for example in quantum harmonic oscillators). Kepler’s Third Law Using even high school knowledge...","categories": [],
-        "tags": ["Classical Mechanics"],
+        "tags": ["Classical Mechanics","Easy"],
         "url": "/KeplerVirial/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Conservations in Physics - Noether's Theorem",
         "excerpt":"This article is built for two types of audiences. The first part is quite involved and assumes knowledge of advanced classical mechanics (for example Lagrangian Dynamics and Variational Principle). The second part is more mild and is targeted for students with some experiences in basic calculus skills. Proof of Noether’s...","categories": [],
-        "tags": ["Classical Mechanics"],
+        "tags": ["Classical Mechanics","Hard"],
         "url": "/NoetherTheorem/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Perturbations - A view on Classical Mechanics and Quantum Mechanics",
         "excerpt":"“Perturbations” have vastly different meanings in classical mechanics and quantum physics. They both mean a small “deviation” from the origin, however. Note: The notes here are very heavily inspired by the notes written by Prof Arjun Berera, who has taught me both Introductory Dynamics (PHYS08052) and Principles of Quantum Mechanics...","categories": [],
-        "tags": ["Classical Mechanics","Quantum Mechanics"],
+        "tags": ["Classical Mechanics","Quantum Mechanics","Hard"],
         "url": "/SmallPerbuations/",
         "teaser": "/assets/images/Teaser.png"
       },{
@@ -85,97 +85,97 @@ var store = [{
       },{
         "title": "Two Interesting Questions related to Free Fall",
         "excerpt":"This post involves two questions related to free falling. The first is about the terminal velocity of human and a plane under turbulent flow, and the second is about what happens if a human drops to the center of the earth. The results are highly fasicinating! Free Fall of objects...","categories": [],
-        "tags": ["Classical Mechanics"],
+        "tags": ["Classical Mechanics","Easy"],
         "url": "/FreeFall/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Two Particles Collision",
         "excerpt":"Two point masses \\(m_1\\) and \\(m_2\\) are spaced apart in a vacuum in space, with no other forces present other than the gravitational force between them. At what time will they meet in the center? With object 1 on the left and object 2 on the right, we can define...","categories": [],
-        "tags": ["Classical Mechanics"],
+        "tags": ["Classical Mechanics","Medium"],
         "url": "/TwoParticles/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "More on Multivariable Calculus",
         "excerpt":"This section covers the content usually omitted in Calculus 3 Area Estimation using Green’s Theorem An example using Generalized Stoke’s Theorem Curl and Divergence in generalized OCCs (Orthogonal Curvilinear Coordinates) Area Estimation using Green’s Theorem I will illustrate this section using an example: An ellipse with semi-major axis a and...","categories": [],
-        "tags": ["Calculus"],
+        "tags": ["Calculus","Medium"],
         "url": "/Multivariable_Calc/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Tensors and its transformations",
         "excerpt":"Here, we introduce the definition of tensors and the visualization behind it. Then, we discuss its transformation to another basis with a worked example. Definition Consider a rotation of the \\(\\{e_i\\}\\) basis (frame \\(S\\)) to the \\(\\{e'_i\\}\\) basis (frame \\(S'\\)). This is called a passive rotation. The components of two...","categories": [],
-        "tags": ["Algebra"],
+        "tags": ["Algebra","Hard"],
         "url": "/TensorTransformations/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Overview of the Two-body Problem",
         "excerpt":"Here I will present the exact solution of the two body problem. Definition of terms First we define the center of mass coordinate, \\(R\\): \\[\\underline{R}=\\frac{m_1 \\underline{r_1}+m_2 \\underline{r_2}}{m_1+m_2}\\] As \\(m_1\\) and \\(m_2\\) are invariant, we can express \\(P\\), the total momentum, in terms of \\(\\dot{R}\\): \\[\\underline{P}=(m_1+m_2)\\dot{R}\\] How does the center of...","categories": [],
-        "tags": ["Research","Classical Mechanics"],
+        "tags": ["Research","Classical Mechanics","Easy"],
         "url": "/SummerResearch/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Self-Initiated Reserach Group Project",
         "excerpt":"Liquid behaviour is hard to analyze and usually requires the Navier Stokes equation, which does not have an analytic solution. With reference to a published paper, we attempt to model liquid behaviour in the low velocity limit using two models: Continuous Model and Double Pendulum Model. Poster We gave a...","categories": [],
-        "tags": ["Research"],
+        "tags": ["Research","Hard"],
         "url": "/ESRAProject/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Julia Sets and the Mandelbrot Set",
         "excerpt":"Here we explore the relationships between Filled-in Julia Sets and the Mandelbrot Set. In this article we consider the function: \\(f_c: z \\mapsto z^2+c\\), where \\(c\\) is a constant. We then define \\(K_c\\), which is also known as the Filled-in Julia Set, as the set of \\(z\\) which are non-escaping...","categories": [],
-        "tags": ["Fractal Geometry"],
+        "tags": ["Fractal Geometry","Medium"],
         "url": "/MandelbrotSet/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Why is Momentum Conserved?",
         "excerpt":"In this article we explore the conservation of momentum using Lagrangian Dynamics. Momentum in Physics Momentum is defined by \\(p=mv\\). It is a vector quantity, meaning the direction matters. The derivative of momentum is force. In an inertial frame, momentum is a conserved quantity, meaning that it is not affected...","categories": [],
-        "tags": ["Classical Mechanics"],
+        "tags": ["Classical Mechanics","Medium"],
         "url": "/MomentumConservation/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Theorema Egregium --- The reason why all maps are wrong",
         "excerpt":"Theorema Egregium states that isometric surfaces have the same gauss(ian) curvature. Equivalently, surfaces with different gauss curvature are not isometric (with same First Fundamental Forms). Gauss Curvature of a Sphere First note that the Gauss Curvature on the whole sphere is uniform. Next notice that the surface area of a...","categories": [],
-        "tags": ["Differential Geometry"],
+        "tags": ["Differential Geometry","Medium"],
         "url": "/TheoremaEgregium/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Euler's Totient function, Reduced Totient function and Carmichael Number",
         "excerpt":"This post is a gentle introduction on the famous theorems in Number Theory. Motivation I have been given this question in my first Mathematics Class on proving theorems, Accelerated Proofs and Problem Solving: Find \\(a\\) such that \\[3^{124} \\equiv a \\space \\text{mod} \\space 100\\] The answer is \\(81\\) upon my...","categories": [],
-        "tags": ["Number Theory"],
+        "tags": ["Number Theory","Easy"],
         "url": "/CarmichaelFunction/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Integration by Laplace Transform",
         "excerpt":"Laplace Transform is usually used to “decompose” a function into its constituents, or to solve differential equations. However, it can also be used to solve integrals. Below, I will show how it can be used to prove: \\[\\int_0^{\\infty} \\frac{\\sin(x)}{x} \\, dx=\\frac{\\pi}{2}\\] Transforming the problem into a Laplace Transformation Problem Observe...","categories": [],
-        "tags": ["Calculus"],
+        "tags": ["Calculus","Easy"],
         "url": "/LaplaceTransforms/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "An Application of the LTE lemma and Fermat's Little Theorem",
         "excerpt":"Here, I am trying to determine all integers \\(n &gt; 1\\) such that \\(\\frac{2^n+1}{n^2}\\) is an integer. First Part Assume \\(a \\neq 2\\) is the smallest prime that divides \\(n\\). Then, since \\(a \\vert 2^n+1\\), \\(a \\vert 2^{2n}-1\\). Also, we know that \\(a \\vert 2^{a-1}-1\\) (Fermat’s Little Theorem) Therefore, \\(a...","categories": [],
-        "tags": ["Number Theory"],
+        "tags": ["Number Theory","Medium"],
         "url": "/IMO/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "An Easy and Brief Introduction to Bose Gas and Fermi Gas",
         "excerpt":"This post is meant to highlight the physical interpretation and results for Bose Gas and Fermi Gas. The level of math is tame to suit audiences with less mathematical exposure. Problem for Ideal Gas By equipartition theorem, \\(E=\\frac{3}{2}kT\\) for \\(1\\) mole of ideal gas. This corresponds to \\(\\frac{3}{2}k\\) for \\(C_V\\),...","categories": [],
-        "tags": ["Quantum Mechanics","Statistical Mechanics"],
+        "tags": ["Quantum Mechanics","Statistical Mechanics","Easy"],
         "url": "/BoseEinstein/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Third Fundamental Form and How to Find it",
         "excerpt":"In this post, we attempt to relate the Third Fundamental Form with the First and Second Fundamental Form . Finding Characteristic Polynomial At each point \\(x\\) of a surface, the tangent space is an inner product space. \\begin{align} (S_xv, w) = (dn(v), w) \\end{align} Now I will use without proof...","categories": [],
-        "tags": ["Algebra","Differential Geometry"],
+        "tags": ["Algebra","Differential Geometry","Hard"],
         "url": "/ThirdFundamentalForm/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Infinite Descent and Vieta's Formulas",
         "excerpt":"Let \\(a\\) and \\(b\\) be positive integers such that \\(ab + 1\\) divides \\(a^2 + b^2\\). Show that \\(\\frac{a^2 + b^2}{ab + 1}\\) is the square of an integer. Assumptions Assume there exist positive integers \\((a, b)\\) for which \\(k = \\frac{a^2 + b^2}{ab + 1}\\), and \\(k\\) is not...","categories": [],
-        "tags": ["Number Theory"],
+        "tags": ["Number Theory","Easy"],
         "url": "/VietaJumping/",
         "teaser": "/assets/images/Teaser.png"
       },{
         "title": "Wilson's Theorem",
         "excerpt":"Wilson’s Theorem provides another way to find prime numbers. Its proof is surprisingly short and elegant. Statement Wilson’s Theorem states that: \\[(n-1)! \\equiv -1 \\space (\\text{mod} \\space n)\\] This means: Any integer \\(n&gt;1\\) is a prime number iff \\((n-1)!+1\\) is divisible by \\(n\\). Proof Inverses in residue classes Since the...","categories": [],
-        "tags": ["Number Theory"],
+        "tags": ["Number Theory","Easy"],
         "url": "/WilsonTheorem/",
         "teaser": "/assets/images/Teaser.png"
       }]
